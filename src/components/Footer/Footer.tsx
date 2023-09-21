b2ac24d040213b8dc2cde439739aaee55e9c451c
Joao Gomes <joaoeduardogomes@gmail.com>
2023-09-21 13:43:56 -0300

    feat: add footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,6 +1,17 @@
+import { Link } from 'react-router-dom';
+import mealIcon from '../../images/mealIcon.svg';
+import drinkIcon from '../../images/drinkIcon.svg';
+
 function Footer() {
   return (
-    <footer>Footer</footer>
+    <footer data-testid="footer">
+      <Link data-testid="drinks-bottom-btn" to="/drinks">
+        <img src={ drinkIcon } alt="Drink Icon" />
+      </Link>
+      <Link data-testid="meals-bottom-btn" to="/meals">
+        <img src={ mealIcon } alt="Meals Icon" />
+      </Link>
+    </footer>
   );
 }
 export default Footer;
