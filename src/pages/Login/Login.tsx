660e9d9605eb8d0ea2cfa78c9c098a788d4f91ed
Vitor Luigi <vitorluigitrabalho@gmail.com>
2023-09-21 20:45:36 -0300

    feat: req2 e 3

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -2,17 +2,20 @@ import { Link } from 'react-router-dom';
 
 function Login() {
   return (
-    <div
-      style={ {
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        height: '100vh',
-      } }
-    >
-      Login
-      <Link to="/meals">Entrar</Link>
-    </div>
+    <main>
+      <form>
+        <input type="text" data-testid="email-input" name="emailUser" />
+        <input type="text" name="password" data-testid="password-input" />
+        <input type="text" />
+        <div>
+          Login
+          <button data-testid="login-submit-btn">
+            Enter
+            <Link to="/meals" />
+          </button>
+        </div>
+      </form>
+    </main>
   );
 }
 export default Login;
