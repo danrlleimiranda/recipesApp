8c3f71f538960044d22f48e8bfbe0433d7b90e53
Vitor Luigi <vitorluigitrabalho@gmail.com>
2023-09-21 22:45:19 -0300

    feat: page login

--- src/utils/isValid.ts
+++ src/utils/isValid.ts
@@ -1,6 +1,6 @@
 export const isValid = (email: string, password: string) => {
   const regexEmail = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-  const validEmail1 = regexEmail.test(email);
+  const validEmail = regexEmail.test(email);
 
   const validation1 = validEmail1;
   const validation2 = password.length > 6;
