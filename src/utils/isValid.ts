2ff47310ed8cea858215731caacdaf6d8cbd1a5f
Vitor Luigi <vitorluigitrabalho@gmail.com>
2023-09-21 22:47:05 -0300

    feat: done page login, just miss tests

--- src/utils/isValid.ts
+++ src/utils/isValid.ts
@@ -2,7 +2,7 @@ export const isValid = (email: string, password: string) => {
   const regexEmail = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
   const validEmail = regexEmail.test(email);
 
-  const validation1 = validEmail1;
+  const validation1 = validEmail;
   const validation2 = password.length > 6;
   const validationAll = (validation1 && validation2) && validation1 && validation2;
   return validationAll;
