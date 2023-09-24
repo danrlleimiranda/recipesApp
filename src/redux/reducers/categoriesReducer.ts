3919018849e751a0b74003d0b1371cb220ef9e1d
Joao Gomes <joaoeduardogomes@gmail.com>
2023-09-24 19:19:14 -0300

    feat: add categoriesReducer

---
+++ src/redux/reducers/categoriesReducer.ts
@@ -0,0 +1,25 @@
+import { AnyAction } from 'redux';
+import { CategoryType } from '../../types';
+import { FETCH_CATEGORIES_SUCCESS } from '../actions';
+
+type InitialStateType = {
+  [key: string]: CategoryType[],
+};
+
+const INITIAL_STATE = {
+  meals: [],
+  drinks: [],
+};
+
+export const categoriesReducer = (
+  state: InitialStateType = INITIAL_STATE,
+  action: AnyAction,
+) => {
+  switch (action.type) {
+    case FETCH_CATEGORIES_SUCCESS: {
+      return { ...state, ...action.payload };
+    }
+    default:
+      return state;
+  }
+};
