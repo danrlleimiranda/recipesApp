ee80e72bc77f0e90ee4a7de3ce365b242cece8e5
Joao Gomes <joaoeduardogomes@gmail.com>
2023-09-26 14:14:04 -0300

    Merge

--- src/components/routes/Router.tsx
+++ src/components/routes/Router.tsx
@@ -1,23 +1,12 @@
 import { Route, Routes } from 'react-router-dom';
-<<<<<<< HEAD
-import Recipes from '../../pages/Recipes/index';
-=======
->>>>>>> 3eb2bc2fb4ed0be3c4b008965b77a38db6033771
 import Profile from '../../pages/Profile/Profile';
 import DoneRecipes from '../../pages/DoneRecipes/DoneRecipes';
 import FavoriteRecipes from '../../pages/FavoriteRecipes/FavoriteRecipes';
 import HeaderLayout from '../../layout/HeaderLayout';
 import FooterLayout from '../../layout/FooterLayout';
 import Login from '../../pages/Login/Login';
-<<<<<<< HEAD
 import RecipeDetails from '../../pages/RecipeDetails/RecipeDetails';
-import MealDetails from '../../pages/Details/MealDetails';
-import DrinkDetails from '../../pages/Details/DrinkDetails';
-import Recipes from '../Recipes/Recipes';
-=======
 import Recipes from '../../pages/Recipes/Recipes';
-import RecipeDetails from '../../pages/RecipeDetails/RecipeDetails';
->>>>>>> 3eb2bc2fb4ed0be3c4b008965b77a38db6033771
 
 function Router() {
   return (
